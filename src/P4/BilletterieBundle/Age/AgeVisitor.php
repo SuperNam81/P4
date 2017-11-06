ca3cca7f9f9405334db76fdb1ff22ca65c706804
Nam <supernam81@gmail.com>
2017-11-06 12:18:52 +0100

    Calcul age-prix toujours en cours. Dates indisponibles dans le DatePicker OK.

--- src/P4/BilletterieBundle/Age/AgeVisitor.php
+++ src/P4/BilletterieBundle/Age/AgeVisitor.php
@@ -7,15 +7,23 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class AgeVisitor
 {
-  private $dateBirth;
+  /* Avec DateBirth en dur dans le Controleur
+  public function ageCalcul($dateBirth)
+  {  
+    $date = new \DateTime(); // date actuelle
+    $age = $date->diff($dateBirth, true)->y;
+    return $age;
+  }
+  */
 
+  /* Avec un tableau de DateBirth */
   public function ageCalcul($dateBirth)
   {  
     $date1 = new \DateTime(); // date actuelle
-    foreach ($dateBirth as $date2) {
-      $age = $date1->diff($date2, true)->y; // le y = nombre d'années
-      //return $age;     
+    foreach ($dateBirth as $dob => $date2) {
+      $age = $date1->diff($date2, true)->y;
+      return $age;
     }
-   
   }  
+
 }
--- src/P4/BilletterieBundle/Controller/BookingController.php
+++ src/P4/BilletterieBundle/Controller/BookingController.php
@@ -43,12 +43,37 @@ class BookingController extends Controller
 		->getRepository('P4BilletterieBundle:Visitor')
 		->findBy(array('booking' => $booking))
 		;
+		
+		foreach ($listVisitors as $dateBirth) {
+			$dateBirth->getDateBirth();
+			$age = $this->container->get('p4_billetterie.age_visitor');
+			$age->ageCalcul($dateBirth);
+		}
+
+		return $this->render('P4BilletterieBundle:Booking:recap.html.twig', array(
+		  'booking' => $booking,
+		  'listVisitors' => $listVisitors,
+		  'dateBirth' => $dateBirth,
+		  'age' => $age,
+		));
+	}
+
+	/*
+	public function recapAction($id, Booking $booking)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$booking = $em->getRepository('P4BilletterieBundle:Booking')->find($id);
+
+		$listVisitors = $em
+		->getRepository('P4BilletterieBundle:Visitor')
+		->findBy(array('booking' => $booking))
+		;
 
 		foreach ($listVisitors as $dateBirth) {
 			$dateBirth->getDateBirth();
 			$age = $this->container->get('p4_billetterie.age_visitor');
 			$age->ageCalcul($dateBirth);
-			//return $age;
 		}
 
 		return $this->render('P4BilletterieBundle:Booking:recap.html.twig', array(
@@ -57,24 +82,43 @@ class BookingController extends Controller
 		  'age' => $age,
 		));
 	}
-	
+	*/
 
-	public function testAction(Request $request)
+	/* Avec un tableau de DateBirth */
+ 	public function testAction($id, Request $request)
 	{
-		$test = new Test();
-		$form   = $this->createForm(TestType::class, $test);
-
-		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) 
-		{
-			$em = $this->getDoctrine()->getManager();
-			$em->persist($test);
-			$em->flush();
-
-			$request->getSession()->getFlashBag()->add('notice', 'Email bien enregistré.');
+		$em = $this->getDoctrine()->getManager();
+		$booking = $em->getRepository('P4BilletterieBundle:Booking')->find($id);
+		$listVisitors = $em
+		->getRepository('P4BilletterieBundle:Visitor')
+		->findBy(array('booking' => $booking))
+		;		
+		/*
+		foreach ($listVisitors as $visitor) {
+			$age = $this->container->get('p4_billetterie.age_visitor')->ageCalcul($visitor->getDateBirth());
 		}
+		*/
+		foreach ($listVisitors as $visitor => $dateBirth) {
+			//$dateBirth->getDateBirth();
+			$age = $this->container->get('p4_billetterie.age_visitor')->ageCalcul($dateBirth);
+		}		
 
 		return $this->render('P4BilletterieBundle:Booking:test.html.twig', array(
-			'form' => $form->createView(),
-		));        
-	}    
+		  'booking' => $booking,
+		  'listVisitors' => $listVisitors,
+		  'age' => $age,
+		));
+	}
+
+	/* DateBirth en dur OK
+	public function testAction(Request $request)
+	{
+		$dateBirth = new \DateTime('1975-03-21');
+		$age = $this->container->get('p4_billetterie.age_visitor')->ageCalcul($dateBirth);
+
+		return $this->render('P4BilletterieBundle:Booking:test.html.twig', array(
+		  'age' => $age,
+		));
+	}
+	*/   
 }
